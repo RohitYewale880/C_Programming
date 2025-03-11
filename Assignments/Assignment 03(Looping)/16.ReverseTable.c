475fe5edefa58f35f4d776cba312385dc980eee7
RohitYewale880 <rohityewale2020@gmail.com>
2025-03-11 11:33:07 +0530

    Assignment 03

---
+++ Assignments/Assignment 03(Looping)/16.ReverseTable.c
@@ -0,0 +1,28 @@
+///Write a program to print the reverse multiplication table of a given number.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0, Cnt = 0, Res = 0;
+
+    printf("Reverse Table of Number => ");
+    scanf("%d",&No);
+
+    printf("\n\n");
+    printf("=========================");
+
+    for(Cnt = 10; Cnt >= 1; Cnt--)
+    {
+        Res = (No * Cnt);
+        printf("\n%3d * %3d = %3d.",No,Cnt,Res);
+    }
+
+    printf("\n=========================");
+
+
+    getch();
+    return 0;
+}
+
