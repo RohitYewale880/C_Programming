7d971d432b713a4ea8b16f461eada341e7511772
RohitYewale880 <rohityewale2020@gmail.com>
2025-03-16 00:15:27 +0530

    Basic

--- 1.Input Output and Variables & Data Types/RadiusOfCircle.c
+++ 1.Input Output and Variables & Data Types/RadiusOfCircle.c
@@ -1,5 +1,5 @@
 /*Write a program which accept radius of circle from user and calculate its area.
-Consider value of PI as 3.14. (Area = PI * Radius * Radius)*/
+Consider value of PI as 3.14. (Area = PI * Radius * Radius)...*/
 
 #include<stdio.h>
 #include<conio.h>
